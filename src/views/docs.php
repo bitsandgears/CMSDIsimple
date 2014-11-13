b5fb029c309e658be8b6bbcdadcd2dbdb4307153
unknown <k.abbema@PC9.goforit.local>
2014-11-13 10:33:40 +0100

    First commit

---
+++ src/views/docs.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		DoIT CMS Documentation
+	</title>
+</head>
+<body style="margin:0px;padding:0px;overflow:hidden">
+
+	<iframe src="../docs/index.html" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe>
+
+</body>
+</html>
